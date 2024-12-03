717e2cb974373724fad31de1ccbd23058d11b19f
zimrustpro <zimrustpro@gmail.com>
2024-12-03 08:39:15 +0200

    Use `anyhow` crate to deal with multiple errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use anyhow::{anyhow, Error};
+
+fn parse_then_send(input: &[u8]) -> Result<(), Error> {
+    let some_str = std::str::from_utf8(input)?;
+    let number = some_str.parse::<i32>()?;
+    send_number(number)?;
+    Ok(())
+}
+
+fn send_number(number: i32) -> Result<(), Error> {
+    if number < 1_000_000 {
+        println!("Number sent!");
+        Ok(())
+    } else {
+        println!("Too large!");
+        Err(anyhow!("Number is too large"))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", parse_then_send(b"nine"));
+    println!("{:?}", parse_then_send(b"10"));
 }
